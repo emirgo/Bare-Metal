41770df88c25ab916ced2739fe4876097f7225c1
Emir <40762681+emirgo@users.noreply.github.com>
2020-06-11 13:13:04 +0200

    Button

--- gpio_input/main.c
+++ gpio_input/main.c
@@ -2,5 +2,24 @@
 
 int main(void)
 {
-
+	RCC->AHB1ENR |= 0x1; // Port A
+	RCC->AHB1ENR |= 0x4; // Port C
+	
+	// Enable PA5 as output
+	GPIOA->MODER |= 0x400;
+	
+	while(1)
+	{
+		// IF PC13 is high
+		if(GPIOC->IDR & 0x2000)
+		{
+			GPIOA->BSRR |= 0x200000;	// Set PA5 high
+		}
+		else
+		{
+			GPIOA->BSRR |= 0x20;	// Set PA5 low
+		}
+	}
+	
+	return 0;
 }
