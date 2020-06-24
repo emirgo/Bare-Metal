54b519daf045ab17987eac0cabdfda2a626ac73a
Emir <40762681+emirgo@users.noreply.github.com>
2020-06-25 00:19:22 +0200

    TX

--- UART/main.c
+++ UART/main.c
@@ -1,23 +1,38 @@
 #include "stm32f4xx.h"                  // Device header
 
 void USART2_init(void);
+void USART_write(char ch);
 
 int main(void)
 {
 	USART2_init();
+	
+	while(1)
+	{
+		USART_write('H');
+		USART_write('N');
+		USART_write('E');
+		USART_write('\n');
+	}
 	
 	return 0;
 }
 
 void USART2_init(void)
 {
-	RCC->AHB1ENR |= 1;	// Enable clock access for USART2 pin (PA2)
 	RCC->APB1ENR |= 0x20000;	//Enable USART2 pin
+	RCC->AHB1ENR |= 1;	// Enable clock access for USART2 pin (PA2)
 	
 	GPIOA->AFR[0] = 0x0700;
 	GPIOA->MODER |= 0x0020; // Set PA2 to alternate function
 	
 	USART2->BRR = 0x0683;		// 9600 @ 16MHz
 	USART2->CR1 = 0x0008;		// Enable TX
-	USART2->CR1 |= 0x20000;		// Enable USART module
+	USART2->CR1 |= 0x2000;		// Enable USART module
+}
+
+void USART_write(char *ch)
+{
+	while(!(USART2->SR & 0x0080)){}
+	USART2->DR = (ch & 0xFF);
 }
