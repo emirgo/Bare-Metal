e334825fc23539f98016bb93de7c324bac29b7b4
Emir <40762681+emirgo@users.noreply.github.com>
2020-07-03 18:48:03 +0200

    Blink on SysTick

---
+++ SysTick/main.c
@@ -0,0 +1,32 @@
+#include "stm32f4xx.h"                  // Device header
+
+void init_led(void);
+
+int main(void)
+{
+	init_led();
+	
+	// Configure SysTick
+	SysTick->LOAD = 3200000 - 1;
+	SysTick->VAL = 0;
+	SysTick->CTRL = 5;
+	
+	while(1)
+	{
+		// Check if count flag is set
+		if(SysTick->CTRL & 0x10000)
+		{
+			GPIOA->ODR ^= 0x20;
+		}
+	}
+	
+	return 0;
+}
+
+void init_led(void)
+{
+	// Enable AHB1 bus
+	RCC->AHB1ENR |= 1;
+	// GPIOA PA5 Output mode
+	GPIOA->MODER |= 0x400;
+}
